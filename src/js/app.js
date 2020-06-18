ac3d2d345a4c35ac8f8d234ec7af0dd1fcb8785e
Dmitryi <dembelchannel@gmail.com>
2020-06-18 13:26:48 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    //init sliders
+    $('.main-wrapper-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+    });
+});
